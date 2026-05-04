c7b36016a6e4f33f3883f9ef89642b3cb1bfc5a5
Diivika <diivika2@yandex.ru>
2026-05-04 16:18:38 +0300

    update

--- add_all.py
+++ add_all.py
@@ -5,8 +5,6 @@ from adds.add_categories import add_categories
 from adds.add_records import add_records
 
 if __name__ == '__main__':
-    insert_administrators()
     insert_barbers()
     insert_users()
     add_categories()
-    add_records()
