6561c357a3f875ed4e9d58fde1ff556638c9d147
artiik2020 <artemkupranov5@gmail.com>
2026-05-07 10:37:58 +0300

    tg fix

--- bot/main.py
+++ bot/main.py
@@ -7,14 +7,20 @@ from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 import os
 
+from sqlalchemy.sql.coercions import expect
+
 load_dotenv()
 api_key = os.getenv("API_KEY")
+PROXY_URL = 'https://t.me/proxy?server=127.0.0.1&port=1080&secret=dd42392b95201fba78d3ee942eb63dce0b'
 dp = Dispatcher()
 
 
 async def main():
-    bot = Bot(token=api_key, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
+    try:
+        bot = Bot(token=api_key, default=DefaultBotProperties(parse_mode=ParseMode.HTML), proxy=PROXY_URL)
+        await dp.start_polling(bot)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
